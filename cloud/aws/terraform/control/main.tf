fe13110693b1590b624331c610aa0e7c1e790e5e
Mikael Sikora <mikael.sikora@hashicorp.com>
2022-05-19 15:44:06 -0700

    Terraform Plan Works

--- cloud/aws/terraform/control/main.tf
+++ cloud/aws/terraform/control/main.tf
@@ -25,14 +25,20 @@ module "my_ip_address" {
 module "hashistack_cluster" {
   source = "../modules/aws-hashistack"
 
-  owner_name         = var.owner_name
-  owner_email        = var.owner_email
-  region             = var.region
-  availability_zones = var.availability_zones
-  ami                = var.ami
-  key_name           = var.key_name
-  allowlist_ip       = ["${module.my_ip_address.stdout}/32"]
-  stack_name         = var.stack_name
+  owner_name              = var.owner_name
+  owner_email             = var.owner_email
+  region                  = var.region
+  availability_zones      = var.availability_zones
+  ami                     = var.ami
+  key_name                = var.key_name
+  stack_name              = var.stack_name
+  server_instance_type    = var.server_instance_type
+  server_count            = var.server_count
+  client_instance_type    = var.client_instance_type
+  client_count            = var.client_count
+  nomad_autoscaler_image  = var.nomad_autoscaler_image
+  
+  allowlist_ip            = (var.allowlist_ip == "" ? ["${module.my_ip_address.stdout}/32"] : var.allowlist_ip)
 }
 
 module "hashistack_jobs" {
