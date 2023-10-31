8ac8fbe2c948e8e71552f64b467c62b5632c6140
Mikael Sikora <89491078+MikaelHashiCorp@users.noreply.github.com>
2023-10-31 10:28:36 -0700

    Revert "Prompt works; but jobs don't, LB?"
    This reverts commit eafcdc57ead78bae9793cc98ede592a6a0ff7a5f.

--- cloud/aws/terraform/control/main.tf
+++ cloud/aws/terraform/control/main.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.6.0"
-  
+  required_version = ">= 0.12"
+
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.72.0"
+      version = "~> 2.65"
     }
   }
 }
